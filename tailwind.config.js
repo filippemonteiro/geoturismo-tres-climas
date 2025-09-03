9d364d51e1232152f3e40fdf20273cea44a2ea36
filippemonteiro <filippemonteiro@outlook.com>
2025-09-03 09:21:45 -0300

    feat: add color variables and style classes in CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        litoral: "#1E88E5",
-        serra: "#388E3C",
-        sertao: "#F57C00",
-      },
       fontFamily: {
-        sans: ["Roboto", "sans-serif"],
-        heading: ["Poppins", "sans-serif"],
-      },
+        sans: ['Roboto', 'sans-serif'],
+        heading: ['Poppins', 'sans-serif'],
+      }
     },
   },
   plugins: [],
-};
+}
